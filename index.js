036ad147148775405707ef6f1e2b7e4c5a2eccf5
kepa <57668544+kepaperez@users.noreply.github.com>
2022-06-04 19:22:56 +0200

    Firts commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http')
+
+const notes =[
+    {
+        "id":1,
+        "content":"Me tengo que ir a clase",
+        "important":true
+    },
+
+    {
+        "id":2,
+        "content":"Me tengo que ir a clase",
+        "important":true
+    },
+
+    {
+        "id":3,
+        "content":"Comparar patatas",
+        "important":false
+    }
+]
+
+const app = http.createServer((request ,response) =>{
+    response.writeHead(200 ,{'Content-Type':'application/json'})
+    response.end(JSON.stringify(notes))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Node Api
+
+### Intalar
+#### Nodemon
+*La d es para que sea una dependencia de desarrollo
+##### Instalar
+```
+npm install nodemon -D
+```
+##### Definir el archivo que al guardar se reinicie el servidor
+```
+./node_modules/.bin/nodemon index.js
+```
